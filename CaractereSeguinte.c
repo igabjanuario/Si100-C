c2a42b2ff2a3cdd150ed33511074245a8e5b9cc0
Gabriela <igabjanuario@hotmail.com>
2023-08-27 16:22:04 -0300

    editing

--- CaractereSeguinte.c
+++ CaractereSeguinte.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 int main(){
-char Caractere;
+    char Caractere;
     printf("Escolha uma tecla e descubra o Caractere Seguinte:\n");//Solicita Caractere
     scanf("%c",&Caractere);//Lê Caractere
     printf("%c",(Caractere+1));//Imprime Caractere Seguinte
