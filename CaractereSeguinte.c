9f1eddd67098cc415beefabf300db32eafeabd15
Gabriela <igabjanuario@hotmail.com>
2023-08-27 15:28:08 -0300

    commit

---
+++ CaractereSeguinte.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+int main(){
+char Caractere;
+    printf("Escolha uma tecla e descubra o Caractere Seguinte:\n");//Solicita Caractere
+    scanf("%c",&Caractere);//Lê Caractere
+    printf("%c",(Caractere+1));//Imprime Caractere Seguinte
+    return 0;
+}
